254700f21e06b66eb998a5adf25f67881a4b847a
Ilya-Zab <anno.201543@icloud.com>
2024-03-26 15:51:28 +0100

    Added a template feautures list

---
+++ wp-content/themes/sierra/template-parts/features.php
@@ -0,0 +1,13 @@
+<div class="features-list">
+  <ul class="d-flex align-content-center justify-content-center features-list__list flex-sm-wrap">
+
+    <?php foreach ($args['items_array'] as $item) : ?>
+      <li class="d-flex flex-column align-items-center">
+        <img src="<?= $item['path'] ?>" alt="wife">
+        <h3 class="accordion__title text-uppercase">
+          <?= $item['title'] ?>
+        </h3>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
--- wp-content/themes/sierra/template-parts/video-hero.php
+++ wp-content/themes/sierra/template-parts/video-hero.php
@@ -1,5 +1,5 @@
 <section class="hero">
-    <video class="hero__video" src="<?= ASSETS_PATH ?>/img/main.mp4" autoplay muted loop></video>
+    <video class="hero__video" src=<?= $args['video_url'] ?> autoplay muted loop></video>
 
     <div class="container hero__container">
         <div class="row h-100">
@@ -8,17 +8,10 @@
                     <h1 class="h1 hero__h1">
                         <?= $args['title'] ?>
                     </h1>
-                    <a href="/" class="btn big-btn btn-light">Book Now</a>
+                    <a href=<?= $args['btn_link'] ?> class="btn big-btn btn-light"><?= $args['btn_text'] ?></a>
                 </div>
             </div>
         </div>
     </div>
 
 </section>
-
-<!-- get_template_part('template-parts/video-hero', null, array(
-    'video_url' => ASSETS_PATH . '/img/main.mp4',
-    'title' => 'Take Flight Today',
-    'btn_link' => '/',
-    'btn_text' => 'Book Now'
-)); -->
