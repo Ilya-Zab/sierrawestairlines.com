3dc98bf38f2f20dbc46e3c704081fc6c212f0e99
Ilya-Zab <anno.201543@icloud.com>
2024-04-03 15:04:00 +0200

    Added target blank for links

--- wp-content/themes/sierra/template-parts/button.php
+++ wp-content/themes/sierra/template-parts/button.php
@@ -5,7 +5,7 @@
     <?php endif ?>
 
     <a href="<?= $args['button_url'] ?>" class="btn
-    <?= $args['is_btn_light'] ? 'btn-light' : 'btn-danger' ?>">
+    <?= $args['is_btn_light'] ? 'btn-light' : 'btn-danger' ?>" target="_blank">
       <?= $args['button_text'] ?>
     </a>
 
