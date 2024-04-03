054d34dbfefed936b4612fbe916d98ebb16c1cb3
Yevhen <camelcaseman@gmail.com>
2024-04-03 15:51:46 +0200

    Added button alignment

--- wp-content/themes/sierra/template-parts/button.php
+++ wp-content/themes/sierra/template-parts/button.php
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 <div class="text-center text-lg-<?= $args['alignment'] ?> carbon-button <?= $args['reduce_offset'] ? 'reduce-offset' : '' ?>">
-  <a href="<?= $args['button_url'] ?>" class="btn
+  <a <?= $args['is_blank'] ? 'target="_blank"' : '' ?> href="<?= $args['button_url'] ?>" class="btn 
     <?= $args['is_btn_light'] ? 'btn-light' : 'btn-danger' ?>">
     <?= $args['button_text'] ?>
   </a>
 </div>
-=======
-<?php if ($args['is_center']) : ?>
-  <div class="text-center carbon-button <?= $args['reduce_offset'] ? 'reduce-offset' : '' ?>">
-  <?php else : ?>
-    <div class="text-sm-center text-lg-start carbon-button <?= $args['reduce_offset'] ? 'reduce-offset' : '' ?>">
-    <?php endif ?>
-
-    <a href="<?= $args['button_url'] ?>" class="btn
-    <?= $args['is_btn_light'] ? 'btn-light' : 'btn-danger' ?>" target="_blank">
-      <?= $args['button_text'] ?>
-    </a>
-
-    </div>
->>>>>>> 05bddada42bdbf61e96df9434894ac9ba67e8a76
