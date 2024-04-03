036286b0cb36520f2523b1412261168fbb1b13a1
Ilya-Zab <anno.201543@icloud.com>
2024-04-03 13:33:58 +0200

    Added new style

--- wp-content/themes/sierra/template-parts/split.php
+++ wp-content/themes/sierra/template-parts/split.php
@@ -1,10 +1,10 @@
 <section class="split-section pb-12">
   <div class="container">
     <div class="row align-items-center <?= $args['is_reversible'] ? "flex-lg-row flex-column-reverse" : "" ?>">
-      <div class="col-12 col-lg-6 split-section__img-bot">
+      <div class="col-12 col-lg-6 <?= !$args['is_reversible'] ? "split-section__img-bot" : "" ?>">
         <?= $args['split'][0] ?>
       </div>
-      <div class="col-12 col-lg-6">
+      <div class="col-12 col-lg-6 <?= $args['is_reversible'] ? "split-section__img-bot" : "" ?>">
         <?= $args['split'][1] ?>
       </div>
     </div>
